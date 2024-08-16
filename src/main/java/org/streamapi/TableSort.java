1245d69cf9823529ac56ddb0a20c31eb16166648
ram <sriram.ravi.r@outlook.com>
2024-08-16 21:02:10 +0530

    filter table using stream

--- src/main/java/org/streamapi/TableSort.java
+++ src/main/java/org/streamapi/TableSort.java
@@ -26,5 +26,37 @@ public class TableSort {
                 .sorted()
                 .collect(Collectors.toList());
         Assert.assertTrue(fruName.equals(sortFruname));
+        //pagination
+        List<String> price;
+        do {
+            price = fruitName
+                    .stream()
+                    .filter(s->s.getText().contains("Beans"))
+                    .map(s->getPriceViggee(s))
+                    .collect(Collectors.toList());
+            price.forEach(System.out::println);
+            if(price.size()<1){
+                driver.findElement(By.xpath(".pagination li:nth-last-child(2)")).click();
+            }
+        }while(price.size()<1);
+    }
+
+    private static String getPriceViggee(WebElement s) {
+        String price=s.findElement(By.xpath(".//following-sibling::td[1]")).getText();
+        return price;
+    }
+
+    @Test
+    public void filterTable(){
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+        String fName="Banana";
+        driver.findElement(By.id("search-field")).sendKeys(fName);
+        List<WebElement> fruitName=driver.findElements(By.xpath("//tr/td[1]"));
+        List<WebElement> funame=fruitName
+                .stream()
+                .filter(s->s.getText().contains(fName))
+                .collect(Collectors.toList());
+        Assert.assertEquals(fruitName.size(),funame.size());
     }
 }
